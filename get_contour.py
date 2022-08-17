a094023d9dda3ee9f0c3df39407c82c37e746afb
do92 <voduydo@gmail.com>
2022-08-17 23:30:27 +0800

    refactored get_contours

--- get_contour.py
+++ get_contour.py
@@ -2,50 +2,35 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-import pickle
-
-# Read in the image, 0 = grayscale
-# im = cv2.imread(img_file, 1)
-# plt.imshow(im)
-
-
-# cv2 uses BGR colors
-# we can convert to RGB using im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-
-
-# read and convert inmage from BGR to RGB
-# fig, axes = plt.subplots(1, 3)
-# for flag, ax in zip([cv2.IMREAD_COLOR, cv2.IMREAD_GRAYSCALE, cv2.IMREAD_UNCHANGED], axes):
-#     im = cv2.imread(img_file, flag)
-#     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-#     ax.imshow(im)
-#     ax.set_title(flag)
-
-# plt.imshow(im)
+import random
 
 # hardcoded based on template image
 CENTER_COORDS = 600, 550
+
 
 def read_img(file_path):
     im = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
     return im
 
 
+def print_img(img):
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(rgb)
+
+
 def find_contours(im):
-    contours, hierarchy = cv2.findContours(im.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(im.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     cv2.drawContours(im, contours, -1, (0, 255, 0), 3)
-
-    # blank = np.zeros_like(im)
-    # cv2.drawContours(blank, contours, -1, (0, 255, 0), 3)
-
-    # plt.imshow(blank)
 
     return contours
 
 
 def shift_center(contours, center_coords=CENTER_COORDS):
+    """
+    align the image with the template
+    """
     M = cv2.moments(contours[0])
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
@@ -62,20 +47,16 @@ def shift_center(contours, center_coords=CENTER_COORDS):
     return contours
     
 
-def plot_on_template(im, fill=True, template_path='./resources/template.jpg'):
+def plot_on_template(im, fill=True, fill_color = (142, 100, 46), template_path='./resources/template.jpg'):
     tpl = cv2.imread(template_path)
-    # tpl = cv2.cvtColor(tpl, cv2.COLOR_BGR2RGB)
-
+    
     contours = find_contours(im)
     contours = shift_center(contours)
-    cv2.drawContours(tpl, contours, -1, (181, 145, 98), thickness=2, lineType=cv2.LINE_AA)
+    cv2.drawContours(tpl, contours, -1, fill_color, thickness=2, lineType=cv2.LINE_AA)
     
     if fill:
-        # grey = (51, 54, 56)
-        silhouette = 142, 100, 46
-        cv2.fillPoly(tpl, pts=contours, color=silhouette)
+        cv2.fillPoly(tpl, pts=contours, color=fill_color)
     
-    # plt.imshow(tpl)
     return tpl
 
 
@@ -90,14 +71,15 @@ def plot_on_template(im, fill=True, template_path='./resources/template.jpg'):
 
 if __name__ == '__main__':
 
-    # img_files = glob.glob('./resources/img_pokemon_png/*.png')
-    img_files = ['./resources/img_pokemon_png/amoonguss.png']
+    img_files = glob.glob('./resources/img_pokemon_png/*.png')
+    img_files = random.sample(img_files, 2)
     output_dir = './resources/img_with_template'
 
     for img_file in img_files:
         tail = img_file.split('/')[-1]
 
         try:
+            print('reading:', img_files)
             im = read_img(img_file)
             result = plot_on_template(im)
 
