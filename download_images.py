7386e0953e4487bcbc36c5bf9f8f2883f996021d
do92 <voduydo@gmail.com>
2022-09-30 09:55:33 +0700

    changed demo pokemon

--- download_images.py
+++ download_images.py
@@ -1,23 +1,31 @@
-import sys
 import requests
 import random
 import time
 import pandas as pd
 
-
-df = pd.read_csv('./resources/pokemon_list.csv')
+try:
+    plist = df = pd.read_csv('./resources/pokemon_list.csv', usecols=['pokemon'])['pokemon'].tolist()
+except:
+    # fallback in case file's not available
+    plist = ['carvanha',
+    'tangrowth',
+    'simisear',
+    'scyther',
+    'tentacruel',
+    'clawitzer',
+    'trevenant',
+    'gardevoir',
+    'venomoth',
+    'perrserker']
 
 if __name__ == '__main__':
-    confirm = input(f'This script will download {len(df)} images. Are you sure?[y/n]: ')
-    if confirm.lower() == 'y':
-        for idx, pokemon_name in enumerate(df['pokemon']):
-            image_url = f'https://img.pokemondb.net/artwork/vector/large/{pokemon_name}.png'
-            
-            print(idx, image_url)
-            img_data = requests.get(image_url).content
-            with open(f'./resources/img_pokemon_png/{pokemon_name}.png', 'wb') as handler:
-                handler.write(img_data)
+    
+    for idx, pokemon_name in enumerate(plist):
+        image_url = f'https://img.pokemondb.net/artwork/vector/large/{pokemon_name}.png'
+        
+        print(idx, image_url)
+        img_data = requests.get(image_url).content
+        with open(f'./resources/img_pokemon_png/{pokemon_name}.png', 'wb') as handler:
+            handler.write(img_data)
 
-            time.sleep(random.random() * 5)
-    else:
-        sys.exit(0)
+        time.sleep(random.random() * 5)
